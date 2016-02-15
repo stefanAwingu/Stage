54fc2cdb8ab64046763c75d8c47ac69ccde9e8f5
Xplendit <jordi.stevens.r1782@student.hogent.be>
2016-02-15 16:00:52 +0100

    script for finance explorer

---
+++ Scripts/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿try {
+  $sysDrive = $env:SystemDrive
+  $financeExplorerPath = "$sysDrive\tools\financeexplorer"
+ 
+  if(test-path $financeExplorerPath) {
+    write-host "Cleaning out the contents of $financeExplorerPath"
+    Remove-Item "$($financeExplorerPath)\*" -recurse -force
+  }
+   
+  Install-ChocolateyZipPackage 'financeexplorer' 'https://github.com/Xplendit/Finance-Explorer-Package/releases/download/test/FinanceExplorerPortable.zip' $financeExplorerPath
+  Install-ChocolateyPath $financeExplorerPath
+ 
+  write-host 'Finance Explorer has been installed.'
+  Write-ChocolateySuccess 'Finance Explorer'
+} catch {
+  Write-ChocolateyFailure 'Finance Explorer' $($_.Exception.Message)
+  throw 
+}
