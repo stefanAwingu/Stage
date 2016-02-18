1ef971f3940ace740a7d799e718dc56896d9f6a1
lucasclaeys <lucas.claeys.t5203@student.hogent.be>
2016-02-18 15:13:40 +0100

    RDscript

--- Scripts/Appserver/RD.ps1
+++ Scripts/Appserver/RD.ps1
@@ -1,10 +1,5 @@
-function resumeAfterBoot{
-    set-location HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce
-    new-itemproperty . MyKey -propertytype String -value "Powershell $PSCommandPath"
-}
-
 #Get FQDN of verander dit en geef FQDN van remote server in
-$serverNaam = (Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
+$serverNaam = "Demo-server.awingu.test"           <#(Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain#>
 $collectionNaam = "RemoteApssCollection"
 
 
@@ -12,17 +7,18 @@ try{
    #Maakt nieuwe Remote Desktop Session role aan met ConnectionBroker, SessionHost en WebAccesServer (gelijk aan quickinstall via GUI)
    #Problem: Kan niet local runnen dus moet vanuit een andere desktop uit hetzelfde domein
    #Problem: WebAccessServer is required en maakt een webserver aan, mag dit??
-   New-RDSessionDeployment -ConnectionBroker $serverNaam `        -SessionHost $serverNaam `        -WebAccessServer $servernaam 
+   New-RDSessionDeployment -ConnectionBroker $serverNaam `        -SessionHost $serverNaam `        -WebAccessServer $servernaam -Verbose
 
    #Restart bij Error van bij ConnectionBroker validation die vaak voorkomt (en opgelost is na 1 restart)
    #ToDo: Automatisch script verder zetten na restart remote server?
    if($Error[0].ToString() -match "The server has reboots pending and needs to be restarted."){
-            resumeAfterBoot
-            Restart-Computer -ComputerName $serverNaam -Force -Verbose   
+            Restart-Computer -ComputerName $serverNaam -Force -Verbose -Wait
+            
+       New-RDSessionDeployment -ConnectionBroker $serverNaam `       -SessionHost $serverNaam `       -WebAccessServer $servernaam -Verbose  
     }
        
    #Maakt eigen Collection aan
-   New-RDSessionCollection -CollectionName $collectionNaam `        -ConnectionBroker $serverNaam `        -SessionHost $serverNaam -ErrorAction Stop
+   New-RDSessionCollection -CollectionName $collectionNaam `        -ConnectionBroker $serverNaam `        -SessionHost $serverNaam -Verbose
    
    #Publisht alle apps met de nodige commandline settings
    New-RDRemoteApp -Alias Acces -DisplayName Acces -FilePath "C:\Program Files (x86)\Microsoft Office\Office15\MSACCESS.EXE" `       -ShowInWebAcces 1 -CollectionName $collectionNaam -ConnectionBroker $serverNaam -CommandLineSetting Allow
