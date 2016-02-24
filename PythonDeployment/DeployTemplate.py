da30c241ec7877f3e6f8b093fc69495a1d0d480a
LucasClaeys <lucas.claeys.t5203@student.hogent.be>
2016-02-24 10:38:06 +0100

    Update DeployTemplate.py

--- PythonDeployment/DeployTemplate.py
+++ PythonDeployment/DeployTemplate.py
@@ -1,40 +1,34 @@
-from azure.mgmt.resource import Deployment
-from azure.mgmt.resource import DeploymentProperties
-from azure.mgmt.resource import DeploymentMode
-from azure.mgmt.resource import ParametersLink
-from azure.mgmt.resource import TemplateLink
 import azure.mgmt.resource
-import azure.mgmt.compute
 import connection
-import azure.common
-import azure.servicemanagement
-import azure.storage
 from azure.mgmt.common import SubscriptionCloudCredentials
 
-deployment_name = 'MyDeployment'
+deployment_name = 'TestDeployPython'
 group_name = 'arm-deployment'
 auth_token = connection.connect()
 subscription_id = connection.subscription()
 creds = SubscriptionCloudCredentials(subscription_id, auth_token)
 
-#compute_client = azure.mgmt.compute.ComputeManagementClient(creds)
 resource_client = azure.mgmt.resource.ResourceManagementClient(creds)
 
-template = TemplateLink(
-    uri='https://raw.githubusercontent.com/Xplendit/Stage/master/PythonDeployment/armTemplate1.json'
+template_link = azure.mgmt.resource.resourcemanagement.ParametersLink(
+        uri='https://raw.githubusercontent.com/Xplendit/Stage/master/PythonDeployment/armTemplate1.json'
 )
+
+parameters_link = azure.mgmt.resource.resourcemanagement.TemplateLink(
+        uri='https://raw.githubusercontent.com/Xplendit/Stage/master/PythonDeployment/Parameters.json'
+)
+
+properties = azure.mgmt.resource.resourcemanagement.DeploymentProperties(
+        mode="incremental",
+        template_link=template_link,
+        parameters_link=parameters_link
+)
+
+deploy_parameter = azure.mgmt.resource.Deployment()
+deploy_parameter.properties=properties
 
 result = resource_client.deployments.create_or_update(
-   group_name,
-   deployment_name,
-   Deployment(
-       properties=DeploymentProperties(
-           mode=DeploymentMode.incremental,
-           template_link=template,
-       )
-   )
+    resource_group_name=group_name,
+    deployment_name=deployment_name,
+    parameters= deploy_parameter
 )
-
-#result = compute_client.virtual_machines.create_or_update(
-#    group_name,
-#)
