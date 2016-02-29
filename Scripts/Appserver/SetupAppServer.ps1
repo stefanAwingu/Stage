fd828c1c84cf93597d7e8a4d79b1d5f151d46399
Xplendit <jordi.stevens.r1782@student.hogent.be>
2016-02-29 14:43:36 +0100

    Update SetupAppServer.ps1

--- Scripts/Appserver/SetupAppServer.ps1
+++ Scripts/Appserver/SetupAppServer.ps1
@@ -1,3 +1,5 @@
+$dnsAddress = "172.26.17.4"
+Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses $dnsAddress
 $domainName = "awingu.test"
 $password = "Hackaton2015" | ConvertTo-SecureString -asPlainText -Force
 $username = "cloud3" 
