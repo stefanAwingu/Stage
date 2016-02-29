09b5f6b726d11476cd309c3beb5c8cc2c064e282
Xplendit <jordi.stevens.r1782@student.hogent.be>
2016-02-29 13:47:04 +0100

    Update SetupAppServer.ps1

--- Scripts/Appserver/SetupAppServer.ps1
+++ Scripts/Appserver/SetupAppServer.ps1
@@ -1,1 +1,3 @@
-mkdir test
+    winrm set winrm/config/client/auth '@{Basic="true"}'
+    winrm set winrm/config/service/auth '@{Basic="true"}'
+    winrm set winrm/config/service '@{AllowUnencrypted="true"}'
