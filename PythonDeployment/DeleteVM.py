25594849bff80f21b677bf488cab594672ba65f7
LucasClaeys <lucas.claeys.t5203@student.hogent.be>
2016-02-29 17:06:46 +0100

    Update DeleteVM.py

--- PythonDeployment/DeleteVM.py
+++ PythonDeployment/DeleteVM.py
@@ -1,4 +1,5 @@
 import azure.mgmt.compute
+import azure.mgmt.network
 import connection
 from azure.mgmt.common import SubscriptionCloudCredentials
 
@@ -9,5 +10,7 @@ subscription_id = connection.subscription()
 creds = SubscriptionCloudCredentials(subscription_id, auth_token)
 
 compute_client = azure.mgmt.compute.ComputeManagementClient(creds)
+network_client = azure.mgmt.network.NetworkResourceProviderClient(creds)
 
 compute_client.virtual_machines.begin_deleting(resource_group_name=group_name,vm_name=vm_name)
+network_client.network_interfaces.delete(group_name, vm_name)
